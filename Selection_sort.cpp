21a2d09459d67b95730b6852a443f46ceaacb70f
Gaurav Mishra <gauravmishra2401@gmail.com>
2023-12-27 00:49:21 +0530

    added selection sort

---
+++ Selection_sort.cpp
@@ -0,0 +1,31 @@
+#include "header.h"
+
+void swap_numbers(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void selection_sort(int *arr, int size) {
+    for (int i = 0; i < size; i++){
+        for(int j = i + 1; j < size; j++) {
+            if(arr[i] > arr[j])
+                swap_numbers(&arr[i], &arr[j]);
+            // display_array(arr, size); // uncomment this to see the iterations
+        }
+    }
+}
+
+int main(){
+    int arr[] = {32,4,5,7,6,43,45,2312,23,345,54,56,24};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    int number = 6;
+
+    cout<< "Before sorting" << endl;
+    display_array(arr, size);
+    selection_sort(arr, size);
+    cout << "After sorting" << endl;
+    display_array(arr, size);
+
+    return 0;
+}
