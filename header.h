7fc2dd089d0c826f97559bc0818abd49bd3b9f48
Gaurav Mishra <gauravmishra2401@gmail.com>
2023-12-27 00:34:59 +0530

    added a display function for a array

--- header.h
+++ header.h
@@ -10,3 +10,10 @@
 #include <list>
 #include <math.h>
 using namespace std;
+
+void display_array(int arr[], int size){
+    for(int i=0; i<size; i++){
+        cout << arr[i] << ", ";
+    }
+    cout << endl;
+}
