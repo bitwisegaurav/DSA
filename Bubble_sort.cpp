30f914463b12b46ca6647b4cf944e33de5785676
Gaurav Mishra <gauravmishra2401@gmail.com>
2023-12-27 00:56:05 +0530

    added bubble sort

---
+++ Bubble_sort.cpp
@@ -0,0 +1,31 @@
+#include "header.h"
+
+void swap_numbers(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void bubble_sort(int *arr, int size) {
+    for (int i = 0; i < size; i++){
+        for(int j = 0; j < size-i-1; j++) {
+            if(arr[j] > arr[j+1])
+                swap_numbers(&arr[j], &arr[j+1]);
+            // display_array(arr, size); // uncomment this to see the iterations
+        }
+    }
+}
+
+int main(){
+    int arr[] = {32,4,5,7,6,43,45,2312,23,345,54,56,24};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    int number = 6;
+
+    cout<< "Before sorting" << endl;
+    display_array(arr, size);
+    bubble_sort(arr, size);
+    cout << "After sorting" << endl;
+    display_array(arr, size);
+
+    return 0;
+}
